637b55d9a34f7592c8360a400ee2d05abfa4215d
Rwan Khalifa <rwankhalifa@Rwans-MacBook-Air.local>
2024-08-04 00:34:19 -0700

    Update firebase.js to use environment variables

--- firebase.js
+++ firebase.js
@@ -3,19 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBPH6bIK9B9WPLSbMKeJYXXlwzCxmFc_rA",
-  authDomain: "inventory-management-c42a3.firebaseapp.com",
-  projectId: "inventory-management-c42a3",
-  storageBucket: "inventory-management-c42a3.appspot.com",
-  messagingSenderId: "801097266767",
-  appId: "1:801097266767:web:a1d3da4f91cd6f09a49c3a",
-  measurementId: "G-YWTF9JTHYS"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
